b1aca568cbff6dd87aa36a91b4b4155b567d4fa4
Aldana Guillermo <galdana@andreani.com>
2023-05-05 16:04:57 -0300

    feat: proyecto inicial

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import './App.css'
+import { getUserById } from './store/actions/userActions';
+
+function App() {
+  const dispatch = useDispatch();
+  const { user } = useSelector(state => state.UserReducer);
+
+  const searchUser = () => {
+    dispatch(getUserById())
+  }
+
+  return (
+    <React.Fragment>
+      <h2>{user.name}</h2>
+      <h6>{user.email}</h6>
+      <button onClick={searchUser}>Extraer usuario</button>
+    </React.Fragment>
+  )
+}
+
+export default App
